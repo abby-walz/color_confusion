3beeabc9a35b9799a23372d1b696ace54bfed946
abby-walz <cs.prairiewolf@gmail.com>
2016-12-01 15:09:25 -0500

    Starting game algorithm
    Figuring out how to make the game work.

---
+++ colorconfusion/game.js
@@ -0,0 +1,27 @@
+// game.js
+// Color Confusion
+
+// algorithm
+// get length and width of the canvas
+// initialize score, number of Cicles, answer, color array 
+// while answer is true
+	// draw number of circles based on score
+	// randomly choose correct word or color 
+	// fill in colors from array in random circles
+	// write matching color names in random circles
+	// remember which is correct
+	// wait for players choice
+	// update answer (true or false)  
+	
+// fuctions to find or make
+// draw circles (int n, int length, int width) 
+	// divide up canvis for n circles
+	// IDK how to deal with odd number 
+	// calculate parameters for circle 
+	// draw circles 
+// write words (int n, int length, int width) 
+	// IDK how to make sure the words are the right size in the circle 
+	//
+// randomly make answer (char type) 
+	// randomly choose a circle 
+	// 
