2d43f8154fef7ea62cbc4c3209b35438afb292c2
abby-walz <cs.prairiewolf@gmail.com>
2016-12-04 20:45:15 -0500

    Got colors working, trying to write game loop

--- colorconfusion/game.js
+++ colorconfusion/game.js
@@ -2,6 +2,13 @@
 // Color Confusion
 
 function color_confusion() {
-	
+	// shuffle word array
+    // shuffle color array
+    // change A-G colors
+    // change wordA-wordG
+    // chose a random number between 0-6
+    // use to know witch is the right answer
+    // right answer: add points loop
+    // wrong answer: game over
 	
 }
