0023bee87416d8409fa32056d6b78f6b358e096b
abby-walz <cs.prairiewolf@gmail.com>
2016-12-04 21:12:38 -0500

    adding variables

--- colorconfusion/game.js
+++ colorconfusion/game.js
@@ -11,4 +11,27 @@ function color_confusion() {
     // right answer: add points loop
     // wrong answer: game over
 	
+    var a = document.getElementById("A");
+    var b = document.getElementById("B");
+    var c = document.getElementById("C");
+    var d = document.getElementById("D");
+    var e = document.getElementById("E");
+    var jumbo = [document.getElementById("A"),                document.getElementById("B"), 
+                  document.getElementById("C"),
+                document.getElementById("D"),
+                 document.getElementById("E"),
+                 document.getElementById("F"),
+                 document.getElementById("G"),
+                 document.getElementById("H"),
+                 document.getElementById("I")];
+    var words = [document.getElementById("wordA"),
+                 document.getElementById("wordB"),
+                 document.getElementById("wordC"),
+                 document.getElementById("wordD"),
+                 document.getElementById("wordE"),
+                 document.getElementById("wordF"),
+                 document.getElementById("wordG"),
+                 document.getElementById("wordH"),
+                 document.getElementById("wordI")];
+    
 }
