b2cc2907c031f280ae56961932c80a5f73934505
Jacklyn Sung <jacklynsung@gmail.com>
2016-12-04 20:47:54 -0500

    game

--- colorconfusion/game.js
+++ colorconfusion/game.js
@@ -1,7 +1,52 @@
 // game.js
 // Color Confusion
 
+/*
+
+var color_possibilities;
+
+color_possibilities["indigo"] = "#FFFFFF"
+
+
+*/
+
+var score = 0;
+var colorArray = ["red", "orange", "yellow", "green", "blue", "purple", "white", "black", "gray", "saddlebrown"];
+
+function gameStart() {
+    alert("why");
+    document.getElementById("rules").style.display="none";
+    document.getElementById("game").style.display="block";
+    document.getElementById("outer").style.display="block";
+}
+
+//wjfiowefjowefjoweijfw
+
+/*
+
 function color_confusion() {
 	
 	
 }
+
+
+
+getMyInt().doSomethingElse()
+
+
+doSomethingElse(getMyInt());
+
+getMyInt(f
+    function doSomethingElse()
+    {
+        //do something
+    }
+)
+
+
+var a = getMyInt();
+if(a == 5)
+    {
+        do something
+    }
+//do other things */
---
+++ colorconfusion/public/javascript/game.js
@@ -0,0 +1,76 @@
+// game.js
+// Color Confusion
+
+/*
+
+var color_possibilities;
+
+color_possibilities["indigo"] = "#FFFFFF"
+
+
+*/
+
+var score = 0;
+var colorArray = ["red", "orange", "yellow", "green", "blue", "purple", "white", "black", "gray", "saddlebrown"];
+//var circleID = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function gameStart() {
+    document.getElementById("rules").style.display = "none";
+    document.getElementById("game").style.display = "block";
+    document.getElementById("outer").style.display = "block";
+    
+    //assign each circle a different color and predetermines answer
+    var rngesus = 0;
+    var luckyNum = Math.floor((Math.random() * 10) + 1);
+    var i;
+    for(i = 0; i < 9; i++) {
+        rngesus = Math.floor((Math.random() * 10) + 1);
+        //circleID[i] = colorArray[rngesus];
+        document.getElementById(i.toString).style.color=colorArray[rngesus]; //changes color of circle
+        if(i == luckyNum) { //sets text
+            if(colorArray[rngesus]=="saddlebrown") {
+                document.getElementById("game").innerHTML = "Color: brown";
+            }
+            else {
+                document.getElementById("game").innerHTML = "Color: " + colorArray[rngesus];
+            }
+        }
+    }
+    
+    //pick a random correct circle from circleID
+    rngesus = Math.floor((Math.random() * 10)+1);
+    var color = circleID[rngesus];
+    
+    
+}
+
+//wjfiowefjowefjoweijfw
+
+/*
+
+function color_confusion() {
+	
+	
+}
+
+
+
+getMyInt().doSomethingElse()
+
+
+doSomethingElse(getMyInt());
+
+getMyInt(f
+    function doSomethingElse()
+    {
+        //do something
+    }
+)
+
+
+var a = getMyInt();
+if(a == 5)
+    {
+        do something
+    }
+//do other things */
